224f87113abc13832d3c7a12b74733b5a29ecdf7
Ryan Fernandez <ryaaanf3@gmail.com>
2021-03-31 17:59:25 -0700

    Experience section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Fade from 'react-reveal';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
 import { Parallax, Background } from 'react-parallax';
 import Container from 'react-bootstrap/Container'
 import BgImage from './assets/img/parallex/background.webp';
@@ -11,6 +12,7 @@ import MyCarousel from "./components/my-carousel/mycarousel.component";
 import TitleMessage from "./components/title-message/titlemessage.component";
 import About from "./pages/about/about.component";
 import Skills from "./pages/skills/skills.component";
+import Experience from "./pages/experience/experience.component";
 
 
 
@@ -20,6 +22,7 @@ const App = () => {
       <MyNavBar />
       <MyCarousel />
       <TitleMessage />
+
       {/* About me Section */}
       <div>
         <Parallax 
@@ -34,14 +37,27 @@ const App = () => {
           </Container>
         </Parallax>
       </div>
+
       {/* Skills section */}
       <div>
           <Container className="container-box rounded">
-            <Fade duration={500}>
+            <Slide bottom duration={500}>
+              <hr />
               <Skills />
-            </Fade>
+            </Slide>
           </Container>
       </div>
+
+      {/* Experience Section */}
+      <div>
+        <Container className="container-box rounded">
+          <Fade duration={1000}>
+            <hr />
+            <Experience />
+          </Fade>
+        </Container>
+      </div>
+
     </div>
   );
 }
---
+++ src/pages/experience/experience.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Container from "react-bootstrap/Container"
+import Jumbotron from "react-bootstrap/Jumbotron"
+import Tilt from "react-tilt"
+import Card from "react-bootstrap/Card"
+import "./experience.styles.css"
+import L_POWER from "../../assets/img/experience/PI_Logo.jpg";
+
+
+
+const Experience = () => {
+    return (
+        <div id="experience">
+            <h1 className="pt-3 text-center font-details-b pb-3">EXPERIENCE</h1>
+            <Jumbotron className="jumbo-style">
+                <Container>
+                    <Tilt options={{ max: 25 }}>
+                        <Card>
+                            <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
+                                <Card.Img variant="top" className="image-resize" src={L_POWER} alt="Power Integrations Logo" />
+                            </Card.Header>
+                            <Card.Body className="d-flex justify-content-center flex-column">
+                                <div>
+                                    <Card.Title className="text-center">IT Intern</Card.Title>
+                                </div>
+                                <div>
+                                    <Card.Text className="text-center style">
+                                        <strong>Duration:</strong> February 2020 - November 2020
+                                        <br/>
+                                        <strong> Description </strong>
+                                        <ul className="text-left">
+                                            <li><strong>Supported</strong> IT team in maintaining hardward, software, and other systems.</li>
+                                            <li><strong>Organized, maintained, and distributed</strong> IT hardware and software resources.
+                                            </li>
+                                            <li><strong>Provided</strong> remote tech support. 
+                                            </li>
+                                        </ul>
+                                    </Card.Text>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Tilt>
+                </Container>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default Experience
