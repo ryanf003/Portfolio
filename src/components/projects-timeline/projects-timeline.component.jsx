4845620314e1c56f97db8a8f807293db3d8b35a8
Ryan Fernandez <ryaaanf3@gmail.com>
2021-04-01 16:22:09 -0700

    Updated projects timeline

--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -7,14 +7,13 @@ import Image from "react-bootstrap/Image";
 
 // skills
 import L_REACT from "../../assets/img/skills/react.svg";
-import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
-import L_REDUX from "../../assets/img/skills/redux.svg";
-import L_HTML5 from "../../assets/img/skills/html-5.svg";
 import L_CSS3 from "../../assets/img/skills/css3.svg";
+import L_HTML5 from "../../assets/img/skills/html-5.svg";
 import L_BOOTSTRAP4 from "../../assets/img/skills/bootstrap-4.svg";
-import L_GIT from "../../assets/img/skills/github-api.svg";
+import L_REDUX from "../../assets/img/skills/redux.svg";
 import L_RyanPhotoPortfolio from "../../assets/img/projects/ryan-photo-portfolio.png"
 import L_PJPhotoPortfolio from "../../assets/img/projects/pj-photo-portfolio.png"
+import L_NuCampSite from "../../assets/img/projects/nucampsite.png"
 
 import "./projects-timeline.styles.css";
 
@@ -113,6 +112,98 @@ const ProjectsTimeline = () => {
                         </div>
                     </ImageEvent>
 
+{/* Project: Nucamp Site */}
+<ImageEvent
+                        date="03/19/2021"
+                        className="text-center"
+                        text="NuCampSite"
+                        src={L_NuCampSite}
+                        alt="NuCampSite" >
+                        <div className="d-flex justify-content-between flex-column mt-1">
+                            <div>
+                                <Accordion>
+                                <Card>
+                                    <Accordion.Toggle
+                                    as={Card.Header}
+                                    eventKey="0"
+                                    className="p-2 text-center accordian-main"
+                                    >
+                                    PROJECT DETAILS
+                                    </Accordion.Toggle>
+
+                                    <Accordion.Collapse eventKey="0" className="text-left">
+                                    <Card.Body>
+                                        <strong>Description:</strong> This is a mock website, created to learn how to use developmental tools.
+                                        <hr />
+                                        <strong>Features:</strong>
+                                        <ul className="list-styles pt-1">
+                                            <li>Powered by Bootstrap.</li>
+                                            <li>Managed by Redux</li>
+                                        </ul>
+                                        <hr />
+                                        <strong>Tech used:</strong>
+                                        <ul>
+                                            <li>
+                                                <span className="p-2">
+                                                <Image
+                                                    src={L_HTML5}
+                                                    alt="HTML 5"
+                                                    rounded
+                                                    className="image-style m-1"
+                                                ></Image>{" "}
+                                                HTML5
+                                                </span>
+                                            </li>
+                                            <li>
+                                                <span className="p-2">
+                                                <Image
+                                                    src={L_CSS3}
+                                                    alt="CSS 3"
+                                                    rounded
+                                                    className="image-style m-1"
+                                                ></Image>{" "}
+                                                CSS3
+                                                </span>
+                                            </li>
+                                            <li>
+                                                <span className="p-2">
+                                                <Image
+                                                    src={L_REACT}
+                                                    alt="React"
+                                                    rounded
+                                                    className="image-style1 m-1"
+                                                ></Image>{" "}
+                                                React
+                                                </span>
+                                            </li>
+                                            <li>
+                                                <span className="p-2">
+                                                <Image
+                                                    src={L_REDUX}
+                                                    alt="Redux"
+                                                    rounded
+                                                    className="image-style1 m-1"
+                                                ></Image>{" "}
+                                                Redux
+                                                </span>
+                                            </li>
+                                        </ul>
+                                    </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                </Accordion>
+                            </div>
+                            <div className="d-flex justify-content-between flex-nowrap text-center">
+                                <UrlButton
+                                href="https://github.com/ryanf003/PJFdezPhotoPortfolio"
+                                target="_blank"
+                                >
+                                SOURCE CODE
+                                </UrlButton>
+                            </div>
+                        </div>
+                    </ImageEvent>
+
                     {/* Project: PJ Fdez Photo Portfolio */}
                     <ImageEvent
                         date="02/19/2021"
@@ -186,21 +277,15 @@ const ProjectsTimeline = () => {
                             </div>
                             <div className="d-flex justify-content-between flex-nowrap text-center">
                                 <UrlButton
-                                href="http://ryan-photo-portfolio.herokuapp.com/home"
-                                target="_blank"
-                                >
-                                SEE LIVE
-                                </UrlButton>
-                                <UrlButton
-                                href="https://github.com/ryanf003/RyanFdezPhotoPortfolio"
+                                href="https://github.com/ryanf003/PJFdezPhotoPortfolio"
                                 target="_blank"
                                 >
                                 SOURCE CODE
                                 </UrlButton>
-                                
                             </div>
                         </div>
                     </ImageEvent>
+
                 </Events>
             </Timeline>
             
